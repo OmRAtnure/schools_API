f3758d622de9163ff08df0a92bca01d924a62978
RuchaAtnure <ruchaatnure@gmail.com>
2025-06-12 22:24:10 +0530

    added port

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
-import mysql from 'mysql2';
-import dotenv from 'dotenv';
+import mysql from "mysql2";
+import dotenv from "dotenv";
 dotenv.config();
 
-const pool= mysql.createPool({
-    host:process.env.HOST,
-    user:process.env.USER,
-    password:process.env.PASSWORD,
-    database:process.env.NAME
+const pool = mysql.createPool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.NAME,
+  port: 3306
 });
 
 export default pool;
