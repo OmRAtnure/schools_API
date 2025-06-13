fa2480de3c214b37f5e973d8d0b9d7656f2d6d01
RuchaAtnure <ruchaatnure@gmail.com>
2025-06-13 11:14:22 +0530

    full

--- db.js
+++ db.js
@@ -2,12 +2,12 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.NAME,
-  port: 24719,
+export const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
   waitForConnections: true,
 });
 
